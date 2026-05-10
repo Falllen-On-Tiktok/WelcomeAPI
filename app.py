861466129fa760ece0176e52572002cd412745ba
Falllen-On-Tiktok <alexlutz0422@proton.me>
2026-05-10 00:13:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def banner():
     )
 
     draw.text(
-        (text_x, text_y + 100),
+        (text_x, text_y + 115),
         "Has Landed at CVIA!",
         fill="white",
         font=small_font
